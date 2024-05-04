629265de9ced6be4befd11483b8039e865e92704
Dominik Pflaum <dome.pflaum@gmail.com>
2024-05-04 19:03:15 +0300

    output kwH you didnt have to pay for

--- ems.py
+++ ems.py
@@ -3,6 +3,9 @@ import optimization as opt
 def main():
     while True:
         opt.adjust_energy_allocations()
+        print("/n")
+        print(f"Total kwH you didnt have to pay for: {opt.total_kwH_covered}")
+        print("/n")
 
 if __name__ == '__main__':
     main()
