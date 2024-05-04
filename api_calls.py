2f95b097e552713485ab294b1aca0e8e138e52ab
Jasmin Fränti <jasmin.franti@edu.turkuamk.fi>
2024-05-04 10:22:48 +0300

    added functions to get system time and time stream

--- api_calls.py
+++ api_calls.py
@@ -1,6 +1,28 @@
 import requests
+import json
 
 id = '6cd466d8-9802-413c-b173-65b81d62ceee'
+
+# System time
+
+def getSystemTime():
+    response = requests.get('https://hackathon.kvanttori.fi/system')
+    time = response.json()['time']
+    print(time)
+    return time
+
+# Streams
+
+def getSystemTimeStream():
+    while True:
+        
+        response = requests.get('https://hackathon.kvanttori.fi/system/stream', stream=True)
+        
+        for line in response.iter_lines():
+            if line:
+                decoded_line = line.decode('utf-8')
+                line_as_json = json.loads(decoded_line)
+                print(line_as_json['time'])
 
 def stream_consumption_data(url):
     response = requests.get(url, stream=True)
@@ -11,4 +33,7 @@ def stream_consumption_data(url):
             print(decoded_line)
             
 # TODO change the URL to use the id variable
-stream_consumption_data('https://hackathon.kvanttori.fi/buildings/6cd466d8-9802-413c-b173-65b81d62ceee/streams/consumption')
+#stream_consumption_data('https://hackathon.kvanttori.fi/buildings/6cd466d8-9802-413c-b173-65b81d62ceee/streams/consumption')
+
+
+getSystemTimeStream()
