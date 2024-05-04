06bd254a55f1128346aaef572ed2a1d4fce20a86
Prashan aka Cyclone <hprashan92@gmail.com>
2024-05-04 09:32:14 +0300

    connection to grids

--- internet.py
+++ internet.py
@@ -1,28 +1,15 @@
 import requests
-import json
 
-# Define the URL to which you want to send the POST request
-url = 'https://hackathon.kvanttori.fi'
+id = "88cf343b-4080-4d30-8de9-dfa905d6eb9b"
 
-uuID = "88cf343b-4080-4d30-8de9-dfa905d6eb9b"
+def stream_consumption_data(building_id):
+    url = f'https://hackathon.kvanttori.fi/buildings/{building_id}/streams/consumption'
+    response = requests.get(url, stream=True)
 
-
-user_type = input("if its Get type 1 if its post type 2 : ")
-
-if 
-
-# Convert the data to JSON format
-json_data = json.dumps(data)
-
-# Set the headers to indicate that the data is in JSON format
-headers = {'Content-Type': 'application/json'}
-
-# Send the POST request with JSON data and headers
-response = requests.post(url, data=json_data, headers=headers)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    print("POST request was successful!")
-    print("Returned message:", response.text)
-else:
-    print("POST request failed with status code:", response.status_code)
+    for line in response.iter_lines():
+        if line:
+            decoded_line = line.decode('utf-8')
+            print(decoded_line)
+            
+# Call the function with the id variable
+stream_consumption_data(id)
