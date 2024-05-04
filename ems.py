6003f7e5f3fd00aa23112c6c7b29abd600ee5324
Dominik Pflaum <dome.pflaum@gmail.com>
2024-05-04 19:46:58 +0300

    add user input  team name to retrieve id and pass id to adjust_energy_allocations function

--- ems.py
+++ ems.py
@@ -1,8 +1,16 @@
+import time
+
 import optimization as opt
+import setup as set
 
 def main():
+    user_input = input("Please enter a value: ")
+    time.sleep(2)
+    # Pass the user input to the setup function
+    id = set.setup(user_input)
+
     while True:
-        opt.adjust_energy_allocations()
+        opt.adjust_energy_allocations(id)
         print("/n")
         print(f"Total kwH you didnt have to pay for: {opt.total_kwH_covered}")
         print("/n")
