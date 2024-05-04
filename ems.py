6ad80d5941fb4a24044826a493038e0f925c1475
Dominik Pflaum <dome.pflaum@gmail.com>
2024-05-04 16:53:04 +0300

    add app and logic files with functionality

---
+++ ems.py
@@ -0,0 +1,8 @@
+import optimization as opt
+
+def main():
+    while True:
+        opt.adjust_energy_allocations()
+
+if __name__ == '__main__':
+    main()
---
+++ optimization.py
@@ -0,0 +1,74 @@
+import requests
+import json
+
+base_url = 'https://hackathon.kvanttori.fi'
+id = 'a918af6e-b5ff-4b18-8961-fdccfd76eea3'
+
+
+def get_consumption():
+    url = f'{base_url}/buildings/{id}/measurements'
+    response = requests.get(url).json()
+    return response['consumer']['consumption'][-1]['value']
+
+
+def get_production():
+    url = f'{base_url}/buildings/{id}/measurements'
+    response = requests.get(url).json()
+    sum_of_production = response['producer']['to_consumption'][-1]['value'] + response['producer']['to_grid'][-1][
+        'value'] + response['producer']['to_storage'][-1]['value']
+    return sum_of_production
+
+
+# print(get_consumption())
+# print(get_production())
+
+def calculate_propotion_of_production_needed_for_consumption():
+    consumption = get_consumption()
+    production = get_production()
+    proportion = round(consumption / production, 2)
+    return proportion
+
+
+#print(calculate_propotion_of_production_needed_for_consumption())
+
+
+def set_production_allocations(consumption, grid, storage):
+    url = f'https://hackathon.kvanttori.fi/buildings/{id}/allocations/production_allocation'
+    data = {"to_consumption": consumption,
+            "to_grid": grid,
+            "to_storage": storage}
+
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        print("Request was successful.")
+        print(f"production allocations were adjusted to: consumption {consumption}, grid: {grid}, storage: {storage}")
+        return data
+    else:
+        print(f"Request failed with status code {response.status_code}.")
+
+
+def get_storage_charge():
+    url = f'https://hackathon.kvanttori.fi/buildings/{id}/measurements/storage'
+    response = requests.get(url).json()
+    return response['charge'][-1]['value']
+
+def adjust_energy_allocations():
+    proportion = calculate_propotion_of_production_needed_for_consumption()
+    storage_charge = get_storage_charge()
+    print(f"Proportion of consumption/production: {proportion}")
+    print(f"Storage charge: {storage_charge}")
+    if proportion <= 1:
+        # consumption is lower than production
+        if storage_charge < 250:
+            # there is space in storage
+            # use all production needed for consumption and send the rest to the storage
+            set_production_allocations(proportion, 0, 1 - proportion)
+        elif storage_charge == 250:
+            # storage is full
+            # use all production for consumption and send rest to grid because storage is full
+            set_production_allocations(proportion, 1 - proportion, 0)
+    else:
+        # production is lower than consumption
+        set_production_allocations(1,0,0)
+
